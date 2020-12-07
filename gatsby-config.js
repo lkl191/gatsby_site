3e7744fd9d2b5b4d0544fbbb87157fb05cf1ace4
llk119 <shigotoyou1985@gmail.com>
2020-12-07 17:43:05 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    //`gatsby-plugin-mdx`,
+  ],
 }
---
+++ src/components/Figure.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Figure = props => {
+    return (
+      <Figure>
+        <h1>Hello</h1>
+      </Figure>
+    )
+  }
