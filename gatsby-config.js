aef83aabe02d360a584ee48bbf5213e16569ff0b
llk119 <shigotoyou1985@gmail.com>
2021-01-28 04:37:36 +0900

    20210128

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
@@ -22,6 +23,21 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `./static/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
+
     //`gatsby-plugin-mdx`
   ],
 }
